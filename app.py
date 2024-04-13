7fbe33577add9549bd39848c690bbafc9e84b92e
Abhishek Upadhyay <upadhyayabhishekkumar45@gmail.com>
2024-04-13 11:55:33 +0530

    model_prediction.py

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
-from flask import Flask,render_template
-from src.exception import CustomException
-from src.logger import logging
-import sys,os
-from src.components.data_ingestion import DataIngestion
+from flask import Flask,request,render_template,jsonify
+from src.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
 
-app=Flask(__name__)
+application=Flask(__name__)
 
+app=application
 
+@app.route('/')
+def home_page():
+    return render_template('index.html')
 
-try:
-    @app.route('/')
-    def index():
-        return render_template("index.html")
+@app.route('/predict',methods=['GET','POST'])
 
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('form.html')
+    
+    else:
+        data=CustomData(
+            carat=float(request.form.get('carat')),
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
+        )
+        final_new_data=data.get_data_as_dataframe()
+        predict_pipeline=PredictPipeline()
+        pred=predict_pipeline.predict(final_new_data)
 
-    @app.route('/get-prediction',methods=['POST'])
-    def get_prediction():
-        return render_template("predict.html")
-        
+        results=round(pred[0],2)
 
+        return render_template('form.html',final_result=results)
+    
 
-
-except Exception as e:
-    raise CustomException(sys,e)
-    # logging.info("Some error Occured")
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-    logging.info()
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -29,3 +29,40 @@ class PredictPipeline:
         except Exception as e:
             logging.info("Exception occured in prediction")
             raise CustomException(e,sys)
+        
+        
+class CustomData:
+    def __init__(self,
+                age:float,
+                sex:str,
+                bmi:float,
+                children:float,
+                smoker:str,
+                region:str,
+                ):
+        
+        self.age=age
+        self.sex=sex
+        self.bmi=bmi
+        self.children=children
+        self.smoker=smoker
+        self.region=region
+
+        
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'age':[self.age],
+                'sex':[self.sex],
+                'bmi':[self.bmi],
+                'children':[self.children],
+                'smoker':[self.smoker],
+                'region':[self.region],
+
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
